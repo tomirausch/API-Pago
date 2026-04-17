ffe9c47d57de3f7c5f75bf06fe28e9c071122904
tomirausch <trausch@frsf.utn.edu.ar>
2026-04-17 19:00:26 -0300

    Proyecto generado a partir de Spring Initializr

---
+++ API_Pago/src/main/java/com/tomas/API_Pago/ApiPagoApplication.java
@@ -0,0 +1,13 @@
+package com.tomas.API_Pago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPagoApplication.class, args);
+	}
+
+}
---
+++ API_Pago/src/test/java/com/tomas/API_Pago/ApiPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomas.API_Pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
