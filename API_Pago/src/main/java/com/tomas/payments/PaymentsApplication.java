8f2c3c4770a703e8585dcc81eeadb7ad119cc350
tomirausch <trausch@frsf.utn.edu.ar>
2026-04-17 19:10:28 -0300

    Redefinimos nombre de artefacto y definimos estructura del proyecto

--- API_Pago/src/main/java/com/tomas/payments/PaymentsApplication.java
+++ API_Pago/src/main/java/com/tomas/payments/PaymentsApplication.java
@@ -1,13 +1,13 @@
-package com.tomas.API_Pago;
+package com.tomas.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiPagoApplication {
+public class PaymentsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiPagoApplication.class, args);
+		SpringApplication.run(PaymentsApplication.class, args);
 	}
 
 }
