1a17dabca77d3d223995808c3eb7ec3f2375ac91
tomirausch <trausch@frsf.utn.edu.ar>
2026-04-19 18:19:49 -0300

    Implementamos caso de uso CreatePayment

--- API_Pago/src/main/java/com/tomas/payments/application/ports/input/CreatePaymentUseCase.java
+++ API_Pago/src/main/java/com/tomas/payments/application/ports/input/CreatePaymentUseCase.java
@@ -1,7 +1,7 @@
 package com.tomas.payments.application.ports.input;
 
-import com.tomas.payments.application.dto.PaymentRequest;
+import com.tomas.payments.domain.model.Payment;
 
 public interface CreatePaymentUseCase {
-    void createPayment(PaymentRequest request);
+    void createPayment(Payment payment);
 }
