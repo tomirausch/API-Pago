5a6f17d466a0b810df54426ddf87f20b4b1dbe6d
tomirausch <trausch@frsf.utn.edu.ar>
2026-04-26 13:08:34 -0300

    Cambios en test

--- API_Pago/src/test/java/com/tomas/API_Pago/ApiPagoApplicationTests.java
+++ API_Pago/src/test/java/com/tomas/API_Pago/ApiPagoApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import com.tomas.payments.PaymentsApplication;
+
+@SpringBootTest(classes = PaymentsApplication.class)
 @ActiveProfiles("test")
 class ApiPagoApplicationTests {
 
